3049090f5a0406e59fc31d7f96aff5de7d8ca2c4
Akeem Moshood <moshoodakeemolayinka@gmail.com>
2022-08-01 16:44:52 -0700

    add Todo logic

--- api/models.py
+++ api/models.py
@@ -12,15 +12,29 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from api.extensions import db
 
 
-class Users(db.Model):
+class CommonAttribute:
+    def save(self):
+        """Save user to db"""
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, params):
+        """Update user"""
+        for attr, value in params.items():
+            setattr(self, attr, value)
+
+        db.session.commit()
+
+
+class Users(db.Model, CommonAttribute):
     """User model"""
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50), nullable=False)
     last_name = db.Column(db.String(50), nullable=False)
-    email = db.Column(db.String(50), nullable=False)
-    username = db.Column(db.String(50), nullable=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=True, unique=True)
     password_hash = db.Column(Text, nullable=False)
     confirmed = db.Column(db.Boolean, nullable=False, default=False)
     active = db.Column(db.Boolean, nullable=False, default=True)
@@ -49,24 +63,11 @@ class Users(db.Model):
     def is_active(self):
         return self.active
 
-    def save(self):
-        """Save user to db"""
-        db.session.add(self)
-        db.session.commit()
-        
-
-    def update(self, params):
-        """Update user"""
-        for attr, value in params.items():
-            setattr(self, attr, value)
-
-        db.session.commit()
-
     def __repr__(self) -> str:
         return f"User {self.first_name}"
 
 
-class ToDo(db.Model):
+class ToDo(db.Model, CommonAttribute):
     """ToDo model"""
     __tablename__ = 'todos'
 
@@ -74,10 +75,43 @@ class ToDo(db.Model):
     name = db.Column(db.String(50), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False,
                            default=datetime.utcnow)
-    completed_at = db.Column(db.DateTime, nullable=False)
-    suspended = db.Column(db.Boolean, nullable=False, default=False)
-    duration = db.Column(db.Time, nullable=False)
-    completed = db.Column(db.Boolean, nullable=True)
+    target_time = db.Column(db.Time, nullable=True)
+    duration = db.Column(db.Time, nullable=True)
+    start_at = db.Column(db.DateTime, nullable=True)
+    completed_at = db.Column(db.DateTime, nullable=True)
+    is_suspended = db.Column(db.Boolean, nullable=False, default=False)
+    is_completed = db.Column(db.Boolean, nullable=True, default=False)
+
+    def start(self):
+        """Method to start the task"""
+        self.start_at = datetime.utcnow()
+
+        db.session.commit()
+
+    def complete(self):
+        """Method to create the time the task was completed."""
+        self.completed_at = datetime.utcnow()
+
+        db.session.commit()
+
+    def suspend(self):
+        """Method to suspend a task"""
+        self.is_suspended = True
+
+        db.session.commit()
+
+    def done(self):
+        """Method to set is_completed to True"""
+        self.is_completed = True
+
+        db.session.commit()
+
+    def time_diff(self):
+        """Time difference between start and completed time"""
+        diff = self.completed_at - self.start_at
+        self.duration = self.completed_at + diff
+
+        db.session.commit()
 
     def __repr__(self) -> str:
         return f"{self.name.capitalize()} task created"
