e7cc5d12b13d52138473be36bcf8808f478ce3fc
Akeem Moshood <moshoodakeemolayinka@gmail.com>
2022-08-25 20:58:29 +0100

    add flask_mail extension

--- api/extensions.py
+++ api/extensions.py
@@ -3,8 +3,9 @@
 from apifairy import APIFairy
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-
+from flask_mail import Mail
 
 apifairy = APIFairy()
 db = SQLAlchemy()
 ma = Marshmallow()
+mail = Mail()
