5e6773811aa9b0b663ffc1919bdcdf4dc3081ccf
Akeem Moshood <moshoodakeemolayinka@gmail.com>
2023-01-22 23:47:59 +0100

    add extension

--- api/extensions.py
+++ api/extensions.py
@@ -4,8 +4,10 @@ from apifairy import APIFairy
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_mail import Mail
+from flask_migrate import Migrate
 
 apifairy = APIFairy()
 db = SQLAlchemy()
 ma = Marshmallow()
 mail = Mail()
+migrate = Migrate()
