8730b028042cf9cf674b4a30adc5968e0fbbc88a
Akeem Moshood <moshoodakeemolayinka@gmail.com>
2023-01-22 23:53:12 +0100

    add sample data

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 # custom import
+from random import choice
+
+from faker import Faker
+
 from api.app import create_app
 from api.models import Users, ToDo
 from api.extensions import db
@@ -8,6 +12,11 @@ from api.schemas.todos_schema import TodoEntrySchema
 app = create_app()
 cli = FlaskGroup(app)
 todo_entry_schema = TodoEntrySchema()
+fake = Faker()
+
+
+activities = ['read', 'market', 'access social media',
+              'visit library', 'grocery store']
 
 
 @cli.command("create_db")
@@ -20,17 +29,19 @@ def create_db():
 @cli.command("add_data")
 def add_data():
     target_time = 0
+    Faker.seed(0)
     for i in range(100):
         target_time = '1:20'
-        params = {'name': 'read', 'target_time': target_time}
-        todo = todo_entry_schema.load(params)
-        db.session.add(todo)
+        params = {'first_name': fake.first_name(), 'last_name': fake.last_name(
+        ), "email": fake.email(), "password": fake.password()}
+        user = Users(**params)
+        db.session.add(user)
     db.session.commit()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(Users=Users, db=db)
+    return dict(Users=Users, db=db, Todo=ToDo)
 
 
 if __name__ == '__main__':
